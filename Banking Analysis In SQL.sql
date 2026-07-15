8ebf8f22574598ec3a2b3dd1cb1c8eefe5d83f7b
Fahim <fahimyadgir07@gmail.com>
2026-07-16 01:12:33 +0530

    perform some sql query

--- Banking Analysis In SQL.sql
+++ Banking Analysis In SQL.sql
@@ -1,2 +1,21 @@
 create database Bank;
 use Bank;
+
+select * from bank_analysis
+where Fraud_Flag = 'Yes';
+
+select count(case when Fraud_Flag='Yes' then 1 end) Frause_flag
+from bank_analysis;
+
+select distinct(Bank_Name)
+from bank_analysis;
+
+select count(*)as total_number_of_customer
+from bank_analysis;
+
+select * from bank_analysis
+where region = 'South'
+order by Transaction_Amount desc;
+
+select avg(Annual_Income_INR )from 
+bank_analysis;
