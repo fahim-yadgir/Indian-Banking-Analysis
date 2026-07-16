4feabb6a7b56da4662e8cd1e3280926156a29452
Fahim <fahimyadgir07@gmail.com>
2026-07-16 23:56:17 +0530

    Perform some Intermediate sql question

--- Banking Analysis In SQL.sql
+++ Banking Analysis In SQL.sql
@@ -57,3 +57,49 @@ where age > 60;
 select transaction_amount,customer_name
 from bank_analysis
 where Transaction_Date BETWEEN '2024-01-01' AND '2024-12-31';
+
+select Region,round(sum(Transaction_Amount),2)as Total_Transaction_Amount
+from bank_analysis
+group by region;
+
+select bank_name,count(Transaction_ID)as total_Transaction
+from bank_analysis
+group by bank_name
+order by total_Transaction desc;
+
+select Branch,sum(Transaction_Amount)as Transaction_Amount
+from bank_analysis
+group by branch
+having sum(Transaction_Amount) >=5000000;
+
+select Loan_Type,Loan_Status,count(*)as total_count
+from bank_analysis
+where Loan_Type is not null 
+group by Loan_Type,Loan_Status
+order by Loan_Type,loan_status;
+
+select region ,round(avg(loan_amount),2)as avg_loan_amount
+from bank_analysis
+where Loan_Status = 'Approved'
+group by region;
+
+select age,
+count(case
+when age < 25 then "Adult"
+when age between 25 and 40 then "Men"
+when age between 40 and 60 then "Senior"
+when age > 60 then "senior_citizon" end)as band
+from bank_analysis
+group by age;
+
+select * from
+(select  Region,
+count(Transaction_Type)as max_count
+from bank_analysis
+group by region)as segment
+order by max_count desc;
+
+select Transaction_Type,count(*)as total_count
+from bank_analysis
+group by Transaction_Type;
+
